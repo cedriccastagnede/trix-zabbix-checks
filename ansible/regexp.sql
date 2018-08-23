68211c6fe0d6d4dc197576111b9599f8146471e2
Dmitry Chirikov <dmitry@chirikov.ru>
2018-08-23 13:34:42 +0200

    Playbooks will be mooved to TrinityX

--- ansible/regexp.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM regexps WHERE regexpid=1000;
-DELETE FROM expressions WHERE expressionid=1000;
-
-INSERT INTO regexps VALUES ( 1000, "Mounts", "");
-INSERT INTO expressions VALUES ( 1000, 1000, ".*", 3, ",", 0 );
-
